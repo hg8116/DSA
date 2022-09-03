b51e2259326123f34299a53506fc63c4f781ff8e
hg8116 <2011.harshgupta@gmail.com>
2022-09-03 17:25:22 +0530

    graph

--- test.java
+++ test.java
@@ -3,7 +3,26 @@ import java.util.Arrays;
 import java.util.List;
 
 class test {
-    public static void main(String[] args) {
+    static int maxNum(int n){
+        int num = 0;
+        for (int i=0; i<n; i++){
+            num = num * 10 + 9;
+        }
 
+        return num;
+    }
+
+    static int mixNum(int n){
+        int num = 1;
+        for (int i=0; i<n-1; i++){
+            num = num * 10;
+        }
+
+        return num;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(maxNum(n));
+        System.out.println(mixNum(n));
     }
 }
