c9e86ee950af764e9ea95dd60449b95652069992
hg8116 <2011.harshgupta@gmail.com>
2022-09-05 18:42:26 +0530

    graph

---
+++ CourseScheduleII210/CourseScheduleII.java
@@ -0,0 +1,48 @@
+// https://leetcode.com/problems/course-schedule-ii/
+
+package CourseScheduleII210;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class CourseScheduleII {
+    public int[] findOrder(int n, int[][] pre) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<Integer>());
+        }
+
+        for (int[] x : pre) {
+            adj.get(x[1]).add(x[0]);
+        }
+
+        int[] indegree = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (Integer it : adj.get(i))
+                indegree[it]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0)
+                q.add(i);
+        }
+
+        int[] topo = new int[n];
+        int i = 0;
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            topo[i++] = curr;
+            for (Integer it : adj.get(curr)) {
+                if (--indegree[it] == 0)
+                    q.add(it);
+            }
+        }
+
+        if (i == n)
+            return topo;
+        return new int[]{};
+    }
+}
--- test.java
+++ test.java
@@ -1,28 +1,56 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 class test {
-    static int maxNum(int n){
-        int num = 0;
-        for (int i=0; i<n; i++){
-            num = num * 10 + 9;
+    public static int[] findOrder(int n, int[][] pre) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            adj.add(new ArrayList<Integer>());
         }
 
-        return num;
-    }
-
-    static int mixNum(int n){
-        int num = 1;
-        for (int i=0; i<n-1; i++){
-            num = num * 10;
+        for(int i=0; i<pre.length; i++){
+            for(int j=0; j<pre[i].length; j++){
+                adj.get(pre[i][0]).add(pre[i][1]);
+            }
         }
 
-        return num;
+//        for (List<Integer> x : adj){
+//            System.out.println(x);
+//        }
+        System.out.println(adj);
+
+        int[] indegree = new int[n];
+        for(int i=0; i<n; i++){
+            for(Integer it: adj.get(i))
+                indegree[it]++;
+        }
+        System.out.println(Arrays.toString(indegree));
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<n; i++){
+            if(indegree[i] == 0)
+                q.add(i);
+        }
+
+        int[] topo = new int[n];
+        int i=0;
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            topo[i++] = curr;
+            for(Integer it: adj.get(curr)){
+                if(--indegree[it] == 0)
+                    q.add(it);
+            }
+        }
+
+        if(i == n)
+            return topo;
+        return new int[]{};
     }
     public static void main(String[] args) {
-        int n = 5;
-        System.out.println(maxNum(n));
-        System.out.println(mixNum(n));
+        int[][] pre = {{1,0},{2,0},{3,1},{3,2}};
+        int n = 4;
+
+        findOrder(4, pre);
+
     }
 }
